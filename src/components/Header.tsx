915eeb3022c46c9f2b4a822fc977bfb862c873f6
2mingyu <tnt01010@naver.com>
2024-10-24 03:23:46 +0900

    prettier-plugin-tailwindcss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header>
-      <h2 className="underline">Header</h2>
+      <h2 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Header
+      </h2>
     </header>
   );
 }
